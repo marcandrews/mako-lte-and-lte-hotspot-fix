411ab841e67964b63e01e4903a6694779f27ff40
Marc Andrews <marc.cg.andrews@gmail.com>
2013-12-11 21:32:14 -0500

    Import v1.3

--- system/addon.d/80-mako-lte.sh
+++ system/addon.d/80-mako-lte.sh
@@ -1,8 +1,5 @@
 #!/sbin/sh
-#--------------------------
-# Nexus 4 4.3 LTE fix v1.1
-#        by *child
-#--------------------------
+# Nexus 4 LTE and LTE hotspot/tethering fix
 
 . /tmp/backuptool.functions
 
@@ -14,17 +11,16 @@ case "$1" in
     echo 'ro.ril.def.preferred.network=10' >> /tmp/build.prop
     mv /tmp/build.prop /system/build.prop
     chmod 644 /system/build.prop
+    
+    # sqlite3 "/data/data/com.android.providers.settings/databases/settings.db" "INSERT INTO global (name, value) VALUES ('tether_dun_required', 0)"
 
     echo '#!/system/bin/sh
-#--------------------------
-# Nexus 4 4.3 LTE fix v1.1
-#        by *child
-#--------------------------
+# Nexus 4 LTE and LTE hotspot/tethering fix
 
 (
-sleep 15
-iptables -D natctrl_FORWARD -j DROP
-iptables -t nat -A natctrl_nat_POSTROUTING -o rmnet_usb0 -j MASQUERADE
+  sleep 15
+  iptables -D natctrl_FORWARD -j DROP
+  iptables -t nat -A natctrl_nat_POSTROUTING -o rmnet_usb0 -j MASQUERADE
 )
 ' > /system/etc/init.d/81makoltetether
     chmod 755 /system/etc/init.d/81makoltetether
