427859732b3af8ffe2fd7e3ca642af3a57b62551
marcandrews <marc.cg.andrews@gmail.com>
2014-11-17 22:31:06 -0500

    Delete de-initialize.sh

--- de-initialize.sh
+++
@@ -1,4 +0,0 @@
-#!/sbin/sh
-grep -Ev 'telephony.lteOnGsmDevice|ro.telephony.default_network|ro.ril.def.preferred.network' /system/build.prop > /tmp/build.prop
-mv /tmp/build.prop /system/build.prop
-chmod 644 /system/build.prop
