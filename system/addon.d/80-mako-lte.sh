adb40df65dbbc2c27ddd1c9423ea3be8bfd3d1d9
marcandrews <marc.cg.andrews@gmail.com>
2014-11-17 22:20:27 -0500

    Update 80-mako-lte.sh

--- system/addon.d/80-mako-lte.sh
+++ system/addon.d/80-mako-lte.sh
@@ -5,15 +5,6 @@
 
 case "$1" in
   restore)
-    grep -Ev 'telephony.lteOnGsmDevice|ro.telephony.default_network|ro.ril.def.preferred.network' /system/build.prop > /tmp/build.prop
-    echo 'telephony.lteOnGsmDevice=1' >> /tmp/build.prop
-    echo 'ro.telephony.default_network=10' >> /tmp/build.prop
-    echo 'ro.ril.def.preferred.network=10' >> /tmp/build.prop
-    mv /tmp/build.prop /system/build.prop
-    chmod 644 /system/build.prop
-    
-    # sqlite3 "/data/data/com.android.providers.settings/databases/settings.db" "INSERT INTO global (name, value) VALUES ('tether_dun_required', 0)"
-
     echo '#!/system/bin/sh
 # Nexus 4 LTE and LTE hotspot/tethering fix
 
